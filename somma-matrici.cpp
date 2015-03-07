e8824a09c6d5126139d183338c1a3bb8e67e9efa
Trek3 <treca98@gmail.com>
2015-03-07 17:58:10 +0100

    Create somma-matrici.cpp

---
+++ somma-matrici.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <iostream>
+
+#define DIM 5000
+#define MAX 10
+
+using namespace std;
+
+void carica(int arr[]){
+          for(int i=0;i<DIM;i++)
+                  arr[i]=rand()%MAX;
+}
+
+void somma(int a[], int b[], int c[]){
+     for(int i=0;i<DIM;i++)
+                  c[i]=a[i]+b[i];
+
+}
+
+void stampa(int a[], int b[], int c[]){
+     for(int i=0;i<DIM;i++)
+             cout<<a[i]<<" + "<<b[i]<<" = "<<c[i]<<endl;    
+}
+
+int main(){
+    int a[DIM];
+    int b[DIM];
+    int c[DIM];
+    
+    carica(a);
+    carica(b);
+    somma(a, b, c);
+    stampa(a, b, c);
+    
+    system("PAUSE");
+        
+}
